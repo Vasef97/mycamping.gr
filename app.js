797b65631e6f70b5028d06784db8b420c2e085fd
Vasef97 <116202606+Vasef97@users.noreply.github.com>
2022-11-28 23:04:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const LocalStrategy = require('passport-local');                    //gia epilog
 const User = require('./models/user');                              //require tou user schema
 const mongoSanitize = require('express-mongo-sanitize');
 //const helmet = require('helmet');
-const dbUrl = '//mongodb://localhost:27017/yelp-camp' || process.env.DB_URL;
+const dbUrl = process.env.DB_URL || '//mongodb://localhost:27017/yelp-camp';
 const MongoDBStore = require("connect-mongo")(session);     //prosoxi! douleuvei me to version 3 connect-mongo mono
 
 const userRoutes = require('./routes/users');                       //require ta routes apo router
