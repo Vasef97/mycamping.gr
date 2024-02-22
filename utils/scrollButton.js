ecba88e8dbe12aaa65f66d079fb9f192b71bb5f8
Vasef97 <116202606+Vasef97@users.noreply.github.com>
2024-02-22 10:27:54 +0200

    Add files via upload

---
+++ utils/scrollButton.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Scroll to the top when the button is clicked
+    var scrollToTopButton = document.getElementById('scroll-to-top-button');
+    scrollToTopButton.addEventListener('click', function () {
+        scrollToTop();
+    });
+
+    function scrollToTop() {
+        var scrollStep = -window.scrollY / (200 / 15);
+        var scrollInterval = setInterval(function () {
+            if (window.scrollY !== 0) {
+                window.scrollBy(0, scrollStep);
+            } else {
+                clearInterval(scrollInterval);
+            }
+        }, 15);
+    }
+});
+
+
+
+
+
